eca21473c6c01815c2440ef24a616e3c8b9a3dc7
Sergey Samchuk <85250561+Eqwe-Wewe@users.noreply.github.com>
2021-06-09 23:27:16 +0600

    Update data_manager.py
    :wrench: fix context manager and try except statements

--- data_manager.py
+++ data_manager.py
@@ -1,37 +1,36 @@
-# -*- coding: utf-8 -*-
-
 import os.path
-import traceback
 import csv
 import json
-import mysql.connector
-from contextlib import contextmanager
+from mysql.connector import connect, Error
 from config import config
 
 
-class database_query:
+class DataBaseQuery:
     def __init__(self, config):
         self.configuration = config
 
     def __enter__(self):
         try:
-            self.conn = mysql.connector.connect(**self.configuration)
+            self.conn = connect(**self.configuration)
             self.cursor = self.conn.cursor()
             return self.cursor
-        except (mysql.connector.Error) as err:
+        except Error as err:
             print(err)
 
     def __exit__(self, exc_type, exc_value, exc_trace):
-        self.conn.commit()
-        self.cursor.close()
-        self.conn.close()
+        try:
+            self.conn.commit()
+            self.cursor.close()
+            self.conn.close()
+        except AttributeError as err:
+            print(f'AttributeError: {err}')
         if exc_type:
             raise exc_type(exc_value)
 
 
 class DataManager:
     def __init__(self):
-        self.insert_data = []  # данные на запись
+        self.insert_data = []
 
     @staticmethod
     def write_csv(rows):
@@ -39,23 +38,28 @@ class DataManager:
             if not os.path.exists('saves/data.csv'):
                 with open('saves/data.csv', 'w', newline='') as file:
                     writer = csv.writer(file, delimiter=';')
-                    writer.writerow(['операции', 'результаты', 'время'])
+                    writer.writerow([
+                        'операции',
+                        'результаты',
+                        'время'])
             with open('saves/data.csv', 'a', newline='') as file:
                 writer = csv.writer(file, delimiter=';')
                 writer.writerows(rows)
                 print('CSV entry successful!')
         except OSError as err:
             print(f'Exception CSV: {err}')
-            with open('err_report/report_csv.txt', 'w') as file:
-                traceback.print_exc(file=file)
 
     @classmethod
     def write_database(self, data):
-        with database_query(config) as self.cursor:
-            self.command = """insert into calculator"""\
-                          """(operation, result, date_of_use)"""\
-                          """values(%s, %s, %s);"""
-            self.cursor.executemany(self.command, data)
+        with DataBaseQuery(config) as self.cursor:
+            self.command = """insert into calculator
+                            (operation, result, date_of_use)
+                            values
+                            (%s, %s, %s);"""
+            try:
+                self.cursor.executemany(self.command, data)
+            except AttributeError as err:
+                print(f'AttributeError: {err}')
 
     @staticmethod
     def write_json(data_to_insert):
@@ -77,16 +81,13 @@ class DataManager:
                 json_data = json.dumps(json_data, indent=3, ensure_ascii=False)
                 file.write(json_data)
                 print('json entry successful!')
-        except OSError:
-            with open('err_report/json_report.txt', 'w') as file:
-                traceback.print_exc(file=file)
+        except OSError as err:
+            print(f'Exception JSON: {err}')
 
     @classmethod
     def write_data(self, data):
         if not os.path.exists('saves'):
             os.mkdir('saves')
-        if not os.path.exists('err_report'):
-            os.mkdir('err_report')
         DataManager.write_json(data)
         DataManager.write_csv(data)
         DataManager.write_database(data)
