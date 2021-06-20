b4f10882beaa23f94569eb5eaf534bfe7be65caf
Sergey Samchuk <85250561+Eqwe-Wewe@users.noreply.github.com>
2021-06-21 03:55:47 +0600

    Update and rename create_table(database).py to create_table.py

---
+++ create_table.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+"""
+Objects starting with 'random' should be replaced with custom objects.
+
+"""
+
+from data_manager import DataBaseQuery
+from root_config import config
+
+
+with DataBaseQuery(config) as cursor:
+    operation = """CREATE TABLE randomdatabase.calculator (id int NOT NULL AUTO_INCREMENT,
+                operation varchar(100) NOT NULL,
+                result varchar(100) DEFAULT NULL,
+                date_of_use datetime DEFAULT NULL,
+                PRIMARY KEY (id)
+                )ENGINE=InnoDB;"""
+    try:
+        cursor.execute(operation)
+    except AttributeError as err:
+        print(f'AttributeError: {err}')
