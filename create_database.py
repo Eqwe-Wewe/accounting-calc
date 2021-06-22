3040c0c9182e9bf250cf850182c94a8a3da5efe0
Sergey Samchuk <85250561+Eqwe-Wewe@users.noreply.github.com>
2021-06-23 05:28:10 +0600

    :scroll: add UTF-8 encoding

--- create_database.py
+++ create_database.py
@@ -10,7 +10,8 @@ from root_config import config
 
 
 with DataBaseQuery(config) as cursor:
-    operation = """CREATE DATABASE IF NOT EXISTS randomdatabase"""
+    operation = """CREATE DATABASE IF NOT EXISTS randomdatabase DEFAULT
+                CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;"""
     try:
         cursor.execute(operation)
     except AttributeError as err:
