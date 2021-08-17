b7189363a9337053cf686b6d7ffe53b2b75c9b12
Sergey Samchuk <85250561+Eqwe-Wewe@users.noreply.github.com>
2021-08-17 22:30:34 +0600

    Update create_table.py

--- create_table.py
+++ create_table.py
@@ -10,12 +10,16 @@ from root_config import config
 
 
 with DataBaseQuery(config) as cursor:
-    operation = """CREATE TABLE randomdatabase.calculator (id int NOT NULL AUTO_INCREMENT,
-                operation varchar(100) NOT NULL,
-                result varchar(100) DEFAULT NULL,
-                date_of_use datetime DEFAULT NULL,
-                PRIMARY KEY (id)
-                )ENGINE=InnoDB;"""
+    operation = """
+                    CREATE TABLE randomdatabase.calculator
+                                 (
+                                 id int NOT NULL AUTO_INCREMENT,
+                                 operation varchar(100) NOT NULL,
+                                 result varchar(100) DEFAULT NULL,
+                                 date_of_use datetime DEFAULT NULL,
+                                 PRIMARY KEY (id)
+                                 )ENGINE=InnoDB;
+                """
     try:
         cursor.execute(operation)
     except AttributeError as err:
