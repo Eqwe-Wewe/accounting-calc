3f894fe2966a53237934ef7fd71e260bbd036d21
Sergey Samchuk <85250561+Eqwe-Wewe@users.noreply.github.com>
2021-08-17 22:29:50 +0600

    Update create_database.py

--- create_database.py
+++ create_database.py
@@ -10,8 +10,13 @@ from root_config import config
 
 
 with DataBaseQuery(config) as cursor:
-    operation = """CREATE DATABASE IF NOT EXISTS randomdatabase DEFAULT
-                CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;"""
+    operation = """
+                    CREATE DATABASE IF NOT EXISTS randomdatabase
+                                                  DEFAULT CHARACTER
+                                                  SET utf8
+                                                  DEFAULT COLLATE
+                                                  utf8_general_ci;
+                """
     try:
         cursor.execute(operation)
     except AttributeError as err:
